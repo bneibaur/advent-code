bf024263b042fafabefa092a0da2a89cb9264cc6
bneibaur <bjneibaur@gmail.com>
2022-12-03 15:53:09 -0700

    Remember some basics.

---
+++ Day1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdlib>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    //Remember some basics.
+
+    int age;
+
+    cout << "Hello World!\n";
+
+    cout << "How old are you?\n";
+    cin >> age;
+
+    cout << "You are "; cout << age; cout << " years old. Very cool.";
+
+    return 0;
+}
